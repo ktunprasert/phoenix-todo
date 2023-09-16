e894aeab239e04cdcfc2d7afd88e452e483c44dc
ktunprasert <ktunprasert@outlook.com>
2023-09-16 13:32:46 +0100

    todo: simplified form and default values

--- lib/hello/todo/item.ex
+++ lib/hello/todo/item.ex
@@ -3,9 +3,9 @@ defmodule Hello.Todo.Item do
   import Ecto.Changeset
 
   schema "items" do
-    field :status, :integer
+    field :status, :integer, default: 0
+    field :person_id, :integer, default: 0
     field :text, :string
-    field :person_id, :integer
 
     timestamps()
   end
@@ -14,6 +14,6 @@ defmodule Hello.Todo.Item do
   def changeset(item, attrs) do
     item
     |> cast(attrs, [:text, :person_id, :status])
-    |> validate_required([:text, :person_id, :status])
+    |> validate_required([:text])
   end
 end
